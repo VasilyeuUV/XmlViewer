ef9f1718904080045171ccc7f79e8ce52c2bfe18
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-12-17 17:10:17 +0300

    Base commands

---
+++ XmlViewer/Infrastructure/Commands/Base/CommandBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace XmlViewer.Infrastructure.Commands.Base
+{
+    internal abstract class CommandBase : ICommand
+    {
+
+        #region Executable
+
+        private bool _executable;
+
+        public bool Executable
+        {
+            get => _executable;
+            set
+            {
+                if (_executable == value) { return; }
+
+                _executable = value;
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+
+        #endregion
+
+        #region ICommand
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        bool ICommand.CanExecute(object parameter) => _executable && CanExecute(parameter);
+
+        void ICommand.Execute(object parameter)
+        {
+            if (CanExecute(parameter)) { Execute(parameter); }
+        }
+
+        #endregion
+
+        protected virtual bool CanExecute(object parameter) => true;
+
+        protected abstract void Execute(object parameter);
+
+
+    }
+}
---
+++ XmlViewer/Infrastructure/Commands/Base/LambdaCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace XmlViewer.Infrastructure.Commands.Base
+{
+    internal class LambdaCommand : CommandBase
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+
+        #region CTOR
+
+        public LambdaCommand(Action execute, Func<bool> canExecute = null) : this(
+          execute: p => execute(),
+          canExecute: canExecute is null ? (Func<object, bool>)null : p => canExecute()
+          )
+        { }
+        public LambdaCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this._execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this._canExecute = canExecute;
+        } 
+
+        #endregion
+
+
+        #region CommandBase
+
+        protected override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        protected override void Execute(object parameter) => _execute(parameter);        
+
+        #endregion
+
+    }
+}

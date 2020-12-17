f8ee6bf3ad733d715dff778472e4231dcbfe8ca7
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-12-17 16:24:11 +0300

    MainWindowViewModel

---
+++ XmlViewer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using XmlViewer.ViewModels.Base;
+
+namespace XmlViewer.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        #region TITLE
+
+        /// <summary> Заголовок окна </summary>
+        private string _title = "Xml Viewver";
+
+        /// <summary> Заголовок окна </summary>
+        public string Title { get => _title; set => Set(ref _title, value); }
+
+        #endregion
+
+
+    }
+}

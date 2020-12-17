159d3f0ef0fdc79ba3457efb30b753d7b232095d
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-12-17 20:31:04 +0300

    OpenFile Dialog

--- XmlViewer/Services/Dialogs/IUserDialog.cs
+++ XmlViewer/Services/Dialogs/IUserDialog.cs
@@ -6,5 +6,14 @@ namespace XmlViewer.Services.Dialogs
 {
     internal interface IUserDialog
     {
+
+        #region OPEN FILE
+        private const string FILTER = "Файлы XML (*.xml)|*.xml";
+
+        bool OpenFile(string title, out string selectedFile, string filter = FILTER);
+
+        bool OpenFiles(string title, out IEnumerable<string> selectedFiles, string filter = FILTER); 
+
+        #endregion
     }
 }
--- XmlViewer/Services/Dialogs/UserDialogService.cs
+++ XmlViewer/Services/Dialogs/UserDialogService.cs
@@ -1,10 +1,58 @@
-﻿using System;
+﻿using Microsoft.Win32;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace XmlViewer.Services.Dialogs
 {
     internal class UserDialogService : IUserDialog
     {
+
+        /// <summary>
+        /// Открыть файл
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="selectedFile"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        public bool OpenFile(string title, out string selectedFile, string filter = "Файлы XML (*.xml)|*.xml")
+        {
+            var fileDialog = new OpenFileDialog()
+            {
+                Title = title,
+                Filter = filter
+            };
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                selectedFile = fileDialog.FileName;
+                return true;
+            }
+            selectedFile = null;
+            return false;
+        }
+
+        /// <summary>
+        /// Открыть несколько файлов
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="selectedFiles"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        public bool OpenFiles(string title, out IEnumerable<string> selectedFiles, string filter = "Файлы XML (*.xml)|*.xml")
+        {
+            var fileDialog = new OpenFileDialog()
+            {
+                Title = title,
+                Filter = filter
+            };
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                selectedFiles = fileDialog.FileNames;
+                return true;
+            }
+            selectedFiles = Enumerable.Empty<string>();
+            return false;
+        }
     }
 }

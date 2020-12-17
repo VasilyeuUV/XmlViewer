5eda9df7a512b72cce6da0d6e98eb8fbeb61bd6c
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-12-17 18:46:14 +0300

    Lang resource

--- XmlViewer/ViewModels/MainWindowViewModel.cs
+++ XmlViewer/ViewModels/MainWindowViewModel.cs
@@ -6,13 +6,14 @@ namespace XmlViewer.ViewModels
     {
         #region TITLE
 
-        /// <summary> Заголовок окна </summary>
-        private string _title = "Xml Viewer";
+        ///// <summary> Заголовок окна </summary>
+        //private string _title = "Xml Viewer";
 
-        /// <summary> Заголовок окна </summary>
-        public string Title { get => _title; set => Set(ref _title, value); }
+        ///// <summary> Заголовок окна </summary>
+        //public string Title { get => _title; set => Set(ref _title, value); }
 
         #endregion
+
 
 
     }
